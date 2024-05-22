ecd487430bd91d11c9bd23ec237b0288a72566e2
AmiraliToori <master.masterblocker@gmail.com>
2024-05-22 17:36:32 +0330

    Add sound when cursor on button
    This feature is not working right now.

--- objects/button.py
+++ objects/button.py
@@ -5,6 +5,10 @@ import pygame as pg
 MAIN_FONT_PATH = "material/font/Pixelify_Sans/PixelifySans-VariableFont_wght.ttf"
 FONT_SIZE = 40
 
+HOVER_SOUND = "material/sounds/effects/SelectBtn.wav"
+CLICK_SOUND = "material/sounds/effects/ClickBtn.wav"
+MAX_TIME_PLAY_SOUND = 1
+
 ANTI_ALIAS = True
 
 class Button:
@@ -24,17 +28,29 @@ class Button:
                                      color)
         self.rect = self.text_box.get_rect(center = (self.x, self.y))
         self.clicked = False
+        self.hover_sound = pg.mixer.Sound(HOVER_SOUND)
+        self.click_sound = pg.mixer.Sound(CLICK_SOUND)
+        self.just_one_time_play = True
         
         
     def draw(self, screen, color: str) -> bool:
+        
         action = False
         pos = pg.mouse.get_pos()
         
         if self.rect.collidepoint(pos):
             
+            
+            self.hover_sound.play(0, MAX_TIME_PLAY_SOUND)
+            
+            
+            
+            
+            
             self.text_box = self.text.render(self.text_input,ANTI_ALIAS, color)
             
             if pg.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.click_sound.play(0, MAX_TIME_PLAY_SOUND)
                 self.clicked = True
                 action = True
             
