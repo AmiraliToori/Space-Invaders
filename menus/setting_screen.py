6ed684897c32e4e99b1b46541018f00351062c2c
AmiraliToori <master.masterblocker@gmail.com>
2024-05-31 22:11:04 +0330

    Add resolution and improve the naming variables and attributes
    Add the resolution function (which is not functional obviosuly for now),
    the work on this part is for after the logical part of the game was
    done.

--- menus/setting_screen.py
+++ menus/setting_screen.py
@@ -5,10 +5,10 @@ from icecream import ic
 sys.path.append('/home/glados/Documents/AmirAli Toori/Lessons/Python/Space-Invaders')
 
 
-from objects.button import SettingButton
+from objects.button import SettingButton, Button
 from objects.text import Text
 from sfx import configuration_volume
-
+from graphic import resolution_setting
 
 # Font
 FONT_PATH = "material/font/Pixelify_Sans/PixelifySans-VariableFont_wght.ttf"
@@ -16,13 +16,13 @@ DEFAULT_FONT_COLOR = "white"
 BACKGROUND_COLOR = "black"
 
 # Setting Screen
-SETTING_TITLE_FONT_SIZE = 50
+SETTING_TITLE_FONT_SIZE = 40
 
 GREEN_HOVER = "green"
 RED_HOVER = "red"
 
-MUSIC_BUTTON_OFFSET = 70
-
+VOLUME_BUTTON_OFFSET = 70
+RESOLUTION_BUTTON_OFFSET = 140
 
 
 volume_adjuster = configuration_volume.ConfigurationVolume()
@@ -33,13 +33,12 @@ class SettingScreen:
     def __init__(self,
                  screen,
                  width: int,
-                 height: int,
-                 screen_number: int) -> None:
+                 height: int) -> None:
         
         self.screen = screen
         self.width = width
         self.height = height
-        self.screen_number = screen_number
+        self.screen_number = 3
         
         
         self.setting_title = Text("Settings",
@@ -48,7 +47,7 @@ class SettingScreen:
                                     DEFAULT_FONT_COLOR,
                                     BACKGROUND_COLOR,
                                     width // 2,
-                                    height * 1 // 5)
+                                    height * 2 // 12)
         
         #########################################################################################
         
@@ -58,7 +57,7 @@ class SettingScreen:
                                     DEFAULT_FONT_COLOR,
                                     BACKGROUND_COLOR,
                                     width * 1 // 4,
-                                    height * 2 // 5)
+                                    height * 6 // 12)
         
         
         self.current_music_volume = Text(f"{volume_adjuster.get_current_music_volume()}",
@@ -67,17 +66,17 @@ class SettingScreen:
                                     DEFAULT_FONT_COLOR,
                                     BACKGROUND_COLOR,
                                     width * 3 // 4,
-                                    height * 2 // 5)
+                                    height * 6 // 12)
         
         self.volume_up_music = SettingButton(f">",
                                     DEFAULT_FONT_COLOR,
-                                    width * 3 // 4 + MUSIC_BUTTON_OFFSET,
-                                    height * 2 // 5)
+                                    width * 3 // 4 + VOLUME_BUTTON_OFFSET,
+                                    height * 6 // 12)
         
         self.volume_down_music = SettingButton(f"<",
                                     DEFAULT_FONT_COLOR,
-                                    width * 3 // 4 - MUSIC_BUTTON_OFFSET,
-                                    height * 2 // 5)
+                                    width * 3 // 4 - VOLUME_BUTTON_OFFSET,
+                                    height * 6 // 12)
         #############################################################################################################
         
         self.sound_volume_label = Text("Sound Volume:",
@@ -86,7 +85,7 @@ class SettingScreen:
                                     DEFAULT_FONT_COLOR,
                                     BACKGROUND_COLOR,
                                     width * 1 // 4,
-                                    height * 3 // 5)
+                                    height * 8 // 12)
         
         self.current_sound_volume = Text(f"{volume_adjuster.get_current_sound_volume()}",
                                     FONT_PATH,
@@ -94,20 +93,54 @@ class SettingScreen:
                                     DEFAULT_FONT_COLOR,
                                     BACKGROUND_COLOR,
                                     width * 3 // 4,
-                                    height * 3 // 5)
+                                    height * 8 // 12)
         
         
         self.volume_up_sound = SettingButton(f">",
                                     DEFAULT_FONT_COLOR,
-                                    width * 3 // 4 + MUSIC_BUTTON_OFFSET,
-                                    height * 3 // 5)
+                                    width * 3 // 4 + VOLUME_BUTTON_OFFSET,
+                                    height * 8 // 12)
         
         self.volume_down_sound = SettingButton(f"<",
                                     DEFAULT_FONT_COLOR,
-                                    width * 3 // 4 - MUSIC_BUTTON_OFFSET,
-                                    height * 3 // 5)
+                                    width * 3 // 4 - VOLUME_BUTTON_OFFSET,
+                                    height * 8 // 12)
+        
+        #################################################################################################3
+        
+        self.resolution_preset_label = Text("Resolution Preset:",
+                                    FONT_PATH,
+                                    SETTING_TITLE_FONT_SIZE,
+                                    DEFAULT_FONT_COLOR,
+                                    BACKGROUND_COLOR,
+                                    width * 1 // 4,
+                                    height * 4 // 12)
+        
+        self.current_resolution_preset = Text(f"{resolution_setting.screen.width} x {resolution_setting.screen.height}",
+                                    FONT_PATH,
+                                    SETTING_TITLE_FONT_SIZE,
+                                    DEFAULT_FONT_COLOR,
+                                    BACKGROUND_COLOR,
+                                    width * 3 // 4,
+                                    height * 4 // 12)
         
         
+        self.increase_resolution = SettingButton(f">",
+                                    DEFAULT_FONT_COLOR,
+                                    width * 3 // 4 + RESOLUTION_BUTTON_OFFSET,
+                                    height * 4 // 12)
+        
+        self.decrease_resolution = SettingButton(f"<",
+                                    DEFAULT_FONT_COLOR,
+                                    width * 3 // 4 - RESOLUTION_BUTTON_OFFSET,
+                                    height * 4 // 12)
+        
+        ############################################################################################
+        
+        self.back_button = Button(f"< BACK",
+                                DEFAULT_FONT_COLOR,
+                                60,
+                                25)
         
     def draw(self) -> None:
         
@@ -133,3 +166,30 @@ class SettingScreen:
         self.volume_up_sound.draw(self.screen, GREEN_HOVER, volume_adjuster.volume_up_sound)
         self.volume_down_sound.draw(self.screen, GREEN_HOVER, volume_adjuster.volume_down_sound)
         
+        ##########################################################################################################################
+        
+        self.resolution_preset_label.draw(self.screen)
+        
+        self.current_resolution_preset.draw(self.screen)
+        self.current_resolution_preset.update(f"{resolution_setting.screen.width} x {resolution_setting.screen.height}")
+        
+        self.increase_resolution.draw(self.screen, GREEN_HOVER, resolution_setting.screen.increase_resolution)
+        self.decrease_resolution.draw(self.screen, GREEN_HOVER, resolution_setting.screen.decrease_resolution)
+        
+        ################################################################################################################################
+        
+        back_button = self.back_button.draw(self.screen, RED_HOVER)
+        
+        if back_button:
+            self.screen_number = 1
+    
+    def update(self) -> int:
+        temp = self.screen_number
+        self.screen_number = 3
+        return temp
+        
+        
+        
+setting = SettingScreen(resolution_setting.screen.display(),
+                        resolution_setting.screen.get_width(),
+                        resolution_setting.screen.get_height())
