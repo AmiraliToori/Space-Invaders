7d1806583de8a0af756fa487c797e9653c4c7876
AmiraliToori <master.masterblocker@gmail.com>
2024-05-31 22:12:08 +0330

    New class for representing the fps option in settings screen.

---
+++ graphic/fps.py
@@ -0,0 +1,18 @@
+
+
+import pygame as pg
+
+
+
+
+
+class Fps:
+    
+    
+    def __init__(self) -> None:
+        self.value = 60
+        
+    def change_fps(self, value) -> None:
+        pass
+    
+    
