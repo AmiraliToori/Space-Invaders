cbaabf09e9d0b3e564579a0a1e8be01f9371a3b4
AmiraliToori <master.masterblocker@gmail.com>
2024-06-01 21:06:35 +0330

    enemy.py

--- objects/enemy.py
+++ objects/enemy.py
@@ -19,6 +19,8 @@ ENEMY_TYPE_3_FRAME_ONE = "material/Icons/enemy/enemy_type3/enemy-type3-frame2.pn
 
 ENEMY_MYSTERY = "material/Icons/enemy/mystery/mystery-frame1.png"
 
+SCALE = 0.05
+
 class Enemy(pg.sprite.Sprite):
     
     
@@ -31,15 +33,52 @@ class Enemy(pg.sprite.Sprite):
         self.y = y
         
         
+        
     def fire(self):
         pass
         
 class EnemyOne(Enemy):
     
     
-    def __init__(self) -> None:
-        pg.sprite.Sprite.__init__(self)
+    def __init__(self,
+                 x: int,
+                 y: int) -> None:
+        super().__init__( x, y)
         
-        image_file = pg.image.load(ENEMY_TYPE_1_FRAME_ONE)
+        self.image_file = pg.image.load(ENEMY_TYPE_1_FRAME_ONE)
         
+        self.image_width = self.image_file.get_width()
+        self.image_height = self.image_file.get_height()
+        
+        self.image = pg.transform.scale(self.image_file, (self.image_height * SCALE, self.image_height * SCALE))
+        
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        
+        self.move_right = True
+        self.move_left = False
+    
+
+    def move_left(self) -> None:
+        self.rect.move_ip(- 0.1, 0)
+    
+    def move_right(self) -> None:
+        self.rect.move_ip(0.1, 0)
+    
+    def move_down(self) -> None:
+        self.rect.move_ip(0, 20)
+    
+    
+    def update(self) -> None:
+        
+        
+        if self.x < screen.get_width():
+            self.rect.move_ip(0.1, 0)
+        else:
+            pass
+            
+        
+        
+    
+            
         
