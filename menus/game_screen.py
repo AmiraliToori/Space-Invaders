142fa864e4dc46a0b81a9a46d9d77947ebe7df97
AmiraliToori <master.masterblocker@gmail.com>
2024-06-02 10:20:46 +0330

    game_screen.py
    Some addition and changes around the game_screen module, which for now
    is incomplete and not works correctly.

--- menus/game_screen.py
+++ menus/game_screen.py
@@ -1,8 +1,16 @@
 
+import pygame as pg
+
 from graphic.resolution_setting import screen
 
 from objects.player import player_group
 from objects.bullet import player_bullet
+from objects.enemy import enemy_gp_one
+from objects.player import player
+
+from sfx.sound_list import sounds
+
+from icecream import ic
 
 class GameScreen:
     
@@ -24,7 +32,19 @@ class GameScreen:
         player_group.draw(self.screen)
         
         player_bullet.update()
-        player_bullet.draw(self.screen)
+        player_bullet.draw(self.screen)    
+        
+        enemy_gp_one.draw(self.screen)
+        
+        
+        if pg.sprite.groupcollide(enemy_gp_one, player_bullet, True, True):
+            sounds.invader_killed_sound.play_sound()
+            player.have_bullet = True
+            
+            # if pg.sprite.groupcollide(enemy_gp_one, player_bullet, True, True):
+            #     for enemy_one in enemy_gp_one:
+                    
+        
         
 game = GameScreen(screen.display(),
                   screen.get_width(),
