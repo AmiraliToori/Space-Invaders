f9c980ef74b47e0099e89385db7314ba58042c47
AmiraliToori <master.masterblocker@gmail.com>
2024-06-02 10:15:00 +0330

    Add InvadersKilled animation

--- sfx/sound_list.py
+++ sfx/sound_list.py
@@ -6,6 +6,7 @@ from .sfx import Sound
 SELECT_SOUND = "material/sounds/effects/SelectBtn.wav"
 CLICK_SOUND = "material/sounds/effects/ClickBtn.wav"
 SHOOT_SOUND = "material/sounds/effects/Shoot.wav"
+INVADER_KILLED_SOUND = "material/sounds/effects/InvaderKilled.wav"
 
 class SoundList:
     
@@ -14,7 +15,9 @@ class SoundList:
         self.select_sound = Sound(SELECT_SOUND)
         self.click_sound = Sound(CLICK_SOUND)
         self.shoot_sound = Sound(SHOOT_SOUND)
-        self.sounds_list = [self.select_sound, self.click_sound, self.shoot_sound]
+        self.invader_killed_sound = Sound(INVADER_KILLED_SOUND)
+        
+        self.sounds_list = [self.select_sound, self.click_sound, self.shoot_sound, self.invader_killed_sound]
     
     def play_select_sound(self) -> None:
         self.select_sound.play_sound()
@@ -24,6 +27,9 @@ class SoundList:
         
     def play_shoot_sound(self) -> None:
         self.shoot_sound.play_sound()
+        
+    def play_invader_killed(self) -> None:
+        self.invader_killed_sound.play_sound()
         
     def change_sound_volume(self,
                             value: float) -> None:
