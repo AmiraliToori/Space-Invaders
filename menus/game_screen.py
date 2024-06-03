6c424d06e9b9077ee6cb7a7eaa3bf3a71288bdbe
AmiraliToori <master.masterblocker@gmail.com>
2024-06-03 21:33:12 +0330

    game_screen.py
    Add the player score text to the upper left corner of the screen, add
    the enemy_bullet which is not funcional for now, and for last addition,
    add the score count.

--- menus/game_screen.py
+++ menus/game_screen.py
@@ -1,15 +1,17 @@
 
 import pygame as pg
+import random
 
 from graphic.resolution_setting import screen
 
-from objects.player import player_group
-from objects.bullet import player_bullet
+from objects.player import player_group, player
+from objects.bullet import player_bullet, enemy_bullet, EnemyBullet
 from objects.enemy import enemy_gp
+from objects.text import Text
 
 from icecream import ic
 
-
+FONT_PATH = "material/font/Pixelify_Sans/PixelifySans-VariableFont_wght.ttf"
 
 class GameScreen:
     
@@ -22,6 +24,14 @@ class GameScreen:
         self.screen = screen
         self.width = width
         self.height = height
+        
+        self.score_label = Text(f"SCORE: {player.score}",
+                                FONT_PATH,
+                                30,
+                                "#06ff06",
+                                "black",
+                                80,
+                                20)
     
     
     
@@ -31,6 +41,14 @@ class GameScreen:
         hits = pg.sprite.groupcollide(enemy_group, player_bullet, False, True)
             
         for enemy in hits:
+            
+            if enemy.enemy_type == 1:
+                player.score += 1
+            elif enemy.enemy_type == 2:
+                player.score += 2
+            elif enemy.enemy_type == 3:
+                player.score += 3
+                
             enemy.is_dead = True
             enemy.set_death_image()
             
@@ -54,10 +72,22 @@ class GameScreen:
         
         self.enemy_gps_collide(enemy_gp)
         
+        self.score_label.draw(self.screen)
+        self.score_label.update(f"SCORE: {player.score}")
+        
+        enemy_bullet.update()
+        enemy_bullet.draw(self.screen)  
         
         
-        
-                
+        for bullet in enemy_bullet.sprites():
+            ic(bullet.x, bullet.y)
+            
+        for enemy in enemy_gp.sprites():
+            fire = random.choices([1,0], [0.1, 99.9])[0]
+            if fire:
+                enemy_bullet.add(EnemyBullet(enemy.rect.x, enemy.rect.y))
+             
+              
         
 game = GameScreen(screen.display(),
                   screen.get_width(),
