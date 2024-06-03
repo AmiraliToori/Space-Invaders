5682e62065b8da3969587a3671381d92a0906fb4
AmiraliToori <master.masterblocker@gmail.com>
2024-06-03 10:14:04 +0330

    bullet.py
    Remove commented code, which is not useful anymore.

--- objects/bullet.py
+++ objects/bullet.py
@@ -51,28 +51,6 @@ class PlayerBullet(pg.sprite.Sprite):
         
         if self.rect.y < 0:
             self.kill()
-            
-        
-
-# class EnemyBullet(pg.sprite.Sprite):
-    
-#     def __init__(self,
-#                  enemy_x: int,
-#                  enemy_y: int) -> None:
-#         pg.sprite.Sprite.__init__(self)
-        
-#         self.x, self.y = ( + player.image.get_width() // 2), (player.player_y + player.image.get_height() // 2)
-    
-#         self.rect = ENEMY_BULLET_IMG.get_rect()
-#         self.rect.center = (self.x, self.y)
-        
-    
-#     def update(self):
-#         self.rect.move_ip(0, 1)
-        
-#         if self.rect.y > screen.get_width():
-#             self.kill()
-        
     
 player_bullet = pg.sprite.Group()
 
