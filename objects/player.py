323651f4555d3bd174d6f979c92d9045623bd6dc
AmiraliToori <master.masterblocker@gmail.com>
2024-06-04 21:39:03 +0330

    player.py
    Add the explosion and death frame for player.

--- objects/player.py
+++ objects/player.py
@@ -3,16 +3,21 @@
 import pygame as pg
 
 from graphic.resolution_setting import screen
+from .tools.image_func import create_image
+
+from sfx. sound_list import sounds
 
 from icecream import ic
 
-
+SCALE = 0.05
 PLAYER_IMAGE = "material/Icons/player/player-frame1.png"
 
 PLAYER_DEATH_FRAME_1 = "material/Icons/player/player-death-frame1.png"
 PLAYER_DEATH_FRAME_2 = "material/Icons/player/player-death-frame2.png"
 
-SCALE = 0.05
+frame_lst = [PLAYER_DEATH_FRAME_1, PLAYER_DEATH_FRAME_2]
+
+enemy_death_frames = [create_image(frame, SCALE) for frame in frame_lst]
 
 class Player(pg.sprite.Sprite):
     
@@ -30,15 +35,16 @@ class Player(pg.sprite.Sprite):
         self.player_y = screen.get_height() * 44 // 48
         
         
-        self.image_file = pg.image.load(PLAYER_IMAGE).convert_alpha()
+        self.image_file = pg.image.load(PLAYER_IMAGE)
         self.image_width = self.image_file.get_width()
         self.image_height = self.image_file.get_height()
         
-        self.image = pg.transform.scale(self.image_file, (self.image_width * SCALE, self.image_height * SCALE))
-        self.rect = self.image.get_rect(center = (self.player_x, self.player_y))
+        self.image = pg.transform.scale(self.image_file, (self.image_width * SCALE, self.image_height * SCALE)).convert_alpha()
+        self.rect = self.image.get_rect()
         self.rect.center = (self.player_x, self.player_y)
         
         self.trigger = False
+        self.is_win = False
         
     
     def update(self):
@@ -53,9 +59,10 @@ class Player(pg.sprite.Sprite):
         if self.player_x > 0:
             self.player_x -= self.speed 
         
-    
     def death(self) -> None:
-        pass
+        self.image = enemy_death_frames[0]
+        sounds.play_player_explosion_sound()
+        
     
 player = Player("PLAYER")
 player_group = pg.sprite.GroupSingle()
