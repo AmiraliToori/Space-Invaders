64825326d81c6e0ec10c15223ba8ace5d9f2afe9
AmiraliToori <master.masterblocker@gmail.com>
2024-06-04 21:35:20 +0330

    main.py
    Add the method of draw and update of leaderboard, pause and victory.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import pygame as pg
 
-from menus import main_screen, setting_screen, intro_screen, game_screen, game_popup
+from menus import main_screen, setting_screen, intro_screen, game_screen, game_popup, leaderboard_surface
 
 from objects.player import player
 from objects.bullet import player_bullet, PlayerBullet
@@ -10,12 +10,9 @@ from objects.enemy import enemy_gp, enemy_box
 from objects.tools.timer import enemies_move_timer
 from objects.tools.pause import pause
 
-
-
 from sfx.sound_list import sounds
 
 from icecream import ic
-
 
 
 # MUSIC FILES PATH
@@ -55,7 +52,8 @@ def main():
             
             # enter leaderboard
             case 4:
-                pass
+                leaderboard_surface.leaderboard_screen.draw()
+                screen_number = leaderboard_surface.leaderboard_screen.update()
                 
             # exit button
             case 5:
@@ -66,10 +64,13 @@ def main():
                 if not pause.pause_state:
                     game_screen.game.draw()
                 else:
-                    game_popup.pause_screen.draw()
-                    screen_number = game_popup.pause_screen.update()
-                    pass
-                
+                    game_popup.pause_popup.draw()
+                    screen_number = game_popup.pause_popup.update()
+                    
+                if player.is_win:
+                    game_popup.victory_popup.draw()
+                    screen_number = game_popup.victory_popup.update()
+                    
             case 7:
                 pass
                 
@@ -103,11 +104,10 @@ def main():
                     pause.change_pause_state()
         
             
-            elif event.type == enemies_move_timer.get_timer_id() and not pause.pause_state:
+            elif event.type == enemies_move_timer.get_timer_id() and not pause.pause_state and screen_number == 6:
                 
                 enemy_gp.update()
                 
-                        
                 enemy_box.update_group(enemy_gp)
                 enemy_box.box_movement()
                 
