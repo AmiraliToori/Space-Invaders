7790267839e4b1467e85b12d3191acee03e58608
AmiraliToori <master.masterblocker@gmail.com>
2024-06-04 17:25:30 +0330

    main.py
    Add the pause surface on the gamescreen, which give the ability to
    player to exit the game.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 
 import pygame as pg
 
-from menus import main_screen, setting_screen, intro_screen, game_screen
+from menus import main_screen, setting_screen, intro_screen, game_screen, game_popup
 
 from objects.player import player
 from objects.bullet import player_bullet, PlayerBullet
 from objects.enemy import enemy_gp, enemy_box
-from objects.timer import enemies_move_timer
+
+from objects.tools.timer import enemies_move_timer
+from objects.tools.pause import pause
+
 
 
 from sfx.sound_list import sounds
@@ -24,7 +27,7 @@ def main():
     pg.init()
     pg.mixer.init()
     
-    
+    clock = pg.time.Clock()
     run = True
     screen_number = 1 # Start screen
     # music_list.MusicList().play_main_title()
@@ -54,14 +57,21 @@ def main():
             case 4:
                 pass
                 
-            
             # exit button
             case 5:
                 run = False
                 
             # the game itself
             case 6:
-                game_screen.game.draw()
+                if not pause.pause_state:
+                    game_screen.game.draw()
+                else:
+                    game_popup.pause_screen.draw()
+                    screen_number = game_popup.pause_screen.update()
+                    pass
+                
+            case 7:
+                pass
                 
         
         keys = pg.key.get_pressed()
@@ -83,16 +93,21 @@ def main():
         
             if event.type == pg.KEYDOWN:
             
-                if event.key == pg.K_SPACE:
+                if event.key == pg.K_SPACE and screen_number == 6 and not pause.pause_state:
                     
                     if len(player_bullet) == 0: #TODO - Add special power (unlimited capacity for bullet) for later
                         sounds.play_shoot_sound()
                         player_bullet.add(PlayerBullet())
+                        
+                elif event.key == pg.K_ESCAPE and screen_number == 6:
+                    pause.change_pause_state()
         
             
-            elif event.type == enemies_move_timer.get_timer_id():
+            elif event.type == enemies_move_timer.get_timer_id() and not pause.pause_state:
                 
                 enemy_gp.update()
+                
+                        
                 enemy_box.update_group(enemy_gp)
                 enemy_box.box_movement()
                 
@@ -100,6 +115,7 @@ def main():
             elif event.type == pg.QUIT:
                 run = False
                 
+        clock.tick(60)
         pg.display.flip()
 
 
