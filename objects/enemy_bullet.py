18bf1959424d5d2e2c6cd23b96d1fa298da9f322
AmiraliToori <master.masterblocker@gmail.com>
2024-06-06 10:58:45 +0330

    enemy_bullet.py
    Separate the enemy_bullet and player_bullet module.

---
+++ objects/enemy_bullet.py
@@ -0,0 +1,38 @@
+
+import pygame as pg
+
+from graphic.resolution_setting import screen
+
+from .tools.image_func import create_image
+
+from icecream import ic
+
+SCALE = 0.03
+
+ENEMY_BULLET_IMG_PATH = "material/Icons/enemy/enemy-bullet.png"
+
+ENEMY_BULLET_IMG = create_image(ENEMY_BULLET_IMG_PATH, SCALE)
+
+class EnemyBullet(pg.sprite.Sprite):
+    
+    def __init__(self,
+                 x: int,
+                 y: int) -> None:
+        pg.sprite.Sprite.__init__(self)
+        
+        self.x = x
+        self.y = y
+        
+        self.image = ENEMY_BULLET_IMG
+        
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        
+    def update(self) -> None:
+        self.rect.move_ip(0, 1)
+        
+        if self.rect.y > 0:
+            self.kill()
+        
+        
+enemy_bullet = pg.sprite.Group()
