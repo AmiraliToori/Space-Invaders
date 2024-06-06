ff091a2c89b64408b90ce57db0894097a84e638f
AmiraliToori <master.masterblocker@gmail.com>
2024-06-06 21:05:10 +0330

    timer.py
    Now we change the specific timing of a timer, change the timing
    attribute with it, to keep it update, also add a method called
    set_to_default which keeps the timer to the initial value when the game
    restarted in anyway.

--- objects/tools/timer.py
+++ objects/tools/timer.py
@@ -15,6 +15,7 @@ class Timer:
                  timing: int) -> None:
         self.timer_id = pg.USEREVENT + id
         self.timing = timing
+        self.default_timing = timing
         
     def set_timer_event(self,
                         loop: int = 0) -> None:
@@ -25,6 +26,10 @@ class Timer:
                             loop = 0) -> None:
         self.timing = new_time
         pg.time.set_timer(self.timer_id, new_time, loop)
+        
+    def set_to_default(self) -> None:
+        self.timing = self.default_timing
+        pg.time.set_timer(self.timer_id, self.default_timing)
     
     
     def get_timer_id(self) -> int:
@@ -35,7 +40,5 @@ class Timer:
 enemies_move_timer = Timer(ENEMIES_MOVEMENT_ID, ENEMIES_MOVEMENT_TIMING)
 enemies_move_timer.set_timer_event(ENEMIES_MOVEMENT_TIMING)
 
-enemies_spawn_timer = Timer(ENEMIES_SPAWN_ID, ENEMIES_SPAWN_TIMING)
-enemies_spawn_timer.set_timer_event(ENEMIES_SPAWN_TIMING)
-
-
+# enemies_spawn_timer = Timer(ENEMIES_SPAWN_ID, ENEMIES_SPAWN_TIMING)
+# enemies_spawn_timer.set_timer_event(ENEMIES_SPAWN_TIMING)
