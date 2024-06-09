8f47eea8ca7fceddd88955794229532efe6b56ae
AmiraliToori <master.masterblocker@gmail.com>
2024-06-09 07:54:58 +0330

    temp.py
    Create a class named Temp, which is used for temproray used.

---
+++ objects/tools/temp.py
@@ -0,0 +1,16 @@
+
+
+
+class Temp:
+    
+    
+    def __init__(self) -> None:
+        self.value = 0
+        
+        
+    def change_value(self,
+                     new_value: int) -> None:
+        self.value = new_value
+        
+temp = Temp()
+temp2 = Temp()
